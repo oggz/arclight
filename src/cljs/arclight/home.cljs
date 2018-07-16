fe87a00cf439bfe446db4894814355ab8ebca703
Alex Doccolo <adoccolo@gmail.com>
2018-07-15 21:02:59 -0400

    life features

--- src/cljs/arclight/home.cljs
+++ src/cljs/arclight/home.cljs
@@ -11,6 +11,7 @@
                               :height 500
                               :size 10
                               :speed 150
+                              :generation 0
                               :cells r-pentamino
                               :ctl (chan 0)}))
 
@@ -34,12 +35,17 @@
                        (prn "Stopped!")
                        (do (<! ctl) (recur))))
             (timeout (:speed @state)) (do
-                            (swap! state update :cells evolve)
+                                        (swap! state update :cells evolve)
+                                        (swap! state update :generation inc)
                             ;; (update-grid!)
                             (recur))))
     ctl))
 ;; (swap! state assoc :size 5)
 ;; (count (:cells @state))
+
+(defn reset-life! [state]
+  (swap! state assoc :cells r-pentamino)
+  (swap! state assoc :generation 0))
 
 (defn home-page [app-state]
   (let [s @state]
@@ -49,12 +55,13 @@
       [:div
        [:button {:on-click #(go (>! go-evolve :pause))} "Pause/Play"]
        [:button {:on-click #(go (>! go-evolve :kill))} "Stop"]
-       [:button {:on-click #(swap! state assoc :cells r-pentamino)} "Reset"]
+       [:button {:on-click #(reset-life! state)} "Reset"]
        [:button {:on-click #(swap! state update :size inc)} "+"]
        [:button {:on-click #(swap! state update :size dec)} "-"]]
-      [:svg {:width (:width s)
-             :height (:height s)
-             :viewBox "-250 -250 500 500"}
+      [:svg.life {:width (:width s)
+                  :height (:height s)
+                  :viewBox "-250 -250 500 500"
+                  :on-click #(prn (.-clientX %))}
        (for [[x y] (:cells s)]
          ^{:key (str [x y])}
          [:rect  {:width (- (:size s) 1)
@@ -63,6 +70,6 @@
                   :x (* (:size s) x)
                   :y (* (:size s) y)}])]
       [:div
-       [:p (str "Cells: " (count (:cells s)))]]]
+       [:p (str "Generation: " (:generation s) " | Cells: " (count (:cells s)))]]]
      [:div.alpha
       [:p "This is an implementation of John Conway's Game of Life in Clojurescript. It begins from the r-pentamino lifeform which evolves into many other lifeforms such as spinners and gliders. Please check out the code on github if you are interested. The link for that is below and this code is in the home.cljs file."]]]))
